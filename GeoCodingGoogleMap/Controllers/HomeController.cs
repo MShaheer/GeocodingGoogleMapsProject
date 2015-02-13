2f103a04f99494165580ae69b969fe812c6b1863
shaheer <shaheernov123@gmail.com>
2015-02-13 19:33:46 +0500

    Geocoding Google Map demo first commit

---
+++ GeoCodingGoogleMap/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using GeocodingGoogleMap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeocodingGoogleMap.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+         
+        [HttpGet]
+        public ActionResult LocationInfo()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LocationInfo(PropertyDetail propertyDetail)
+        {
+
+            return RedirectToAction("Index");
+        }
+
+
+	}
+}
---
+++ GeoCodingGoogleMap/Models/PropertyDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeocodingGoogleMap.Models
+{
+    public class PropertyDetail
+    {
+        public int ID { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public int PropertyId { get; set; }
+    }
+}
